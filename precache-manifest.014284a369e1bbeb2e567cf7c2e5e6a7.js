7dd1d726211947c230fd5941f55f4b064974f35a
Fernando Silva <fdograph@gmail.com>
2020-02-23 16:20:45 +0100

    Updates

---
+++ precache-manifest.014284a369e1bbeb2e567cf7c2e5e6a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8903a90e7644f4b121fec602fac3bf",
+    "url": "/rcubeception/index.html"
+  },
+  {
+    "revision": "3d6a305abec0ebda6990",
+    "url": "/rcubeception/static/js/2.b349686c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/rcubeception/static/js/2.b349686c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b98a76c922bc23483c",
+    "url": "/rcubeception/static/js/main.dfe9bc11.chunk.js"
+  },
+  {
+    "revision": "a3c4741be234967dc4f3",
+    "url": "/rcubeception/static/js/runtime-main.e1086353.js"
+  }
+]);
